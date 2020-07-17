823e1263ed7b5b5627dbdea4dd0aac528bc88f1e
SaidiFawzi <fawzi.saidijr@gmail.com>
2020-07-17 09:53:53 +0100

    Updates

---
+++ precache-manifest.d17a7fb66d48b9083e572e719e3d0410.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d386b3234ec8324296500d0cb9271b0",
+    "url": "/checkpoint-react/index.html"
+  },
+  {
+    "revision": "6a20e45c145245060cef",
+    "url": "/checkpoint-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e2d48019f03ad87942e4",
+    "url": "/checkpoint-react/static/css/main.45fbff28.chunk.css"
+  },
+  {
+    "revision": "6a20e45c145245060cef",
+    "url": "/checkpoint-react/static/js/2.30031b91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/checkpoint-react/static/js/2.30031b91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2d48019f03ad87942e4",
+    "url": "/checkpoint-react/static/js/main.be942550.chunk.js"
+  },
+  {
+    "revision": "cad2443a69def824954e",
+    "url": "/checkpoint-react/static/js/runtime-main.2cfb262f.js"
+  }
+]);
